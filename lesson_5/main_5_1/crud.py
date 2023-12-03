45f6bfd2e440d006f72e5847ef7b7754ce400b24
GavrikGal <gavrik.gal@gmail.com>
2023-12-03 11:08:31 +0300

    Урок 5.1 пройден

---
+++ lesson_5/main_5_1/crud.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_todo(db: Session, todo_id: int):
+    return db.query(models.ToDo).filter(models.ToDo.id == todo_id).first()
+
+
+def create_todo(db: Session, todo: schemas.ToDoCreate):
+    db_todo = models.ToDo(**todo.model_dump())
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+
+def update_todo(db: Session, todo: schemas.ToDoUpdate):
+    db_todo = get_todo(db=db, todo_id=todo.id)
+    db_todo.completed = todo.completed
+    # db_todo.update({'completed': todo.completed})
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+
+def delete_todo(db: Session, todo_id: int):
+    db.query(models.ToDo).filter(models.ToDo.id == todo_id).delete()
+    db.commit()
---
+++ lesson_5/main_5_1/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Экземпляр БД, который будет зависимостью для эндпоинтов
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/todo/', response_model=schemas.ToDo)
+def create_todo(todo: schemas.ToDoCreate, db: Session = Depends(get_db)):
+    return crud.create_todo(db=db, todo=todo)
+
+
+@app.get('/todo/{todo_id}', response_model=schemas.ToDo)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db=db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail='ToDo not found')
+    return db_todo
+
+
+@app.put('/todo/', response_model=schemas.ToDo)
+def update_todo(todo: schemas.ToDoUpdate, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db=db, todo_id=todo.id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail='ToDo not found')
+    return crud.update_todo(db=db, todo=todo)
+
+
+@app.delete('/todo/{todo_id}')
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db=db, todo_id=todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail='ToDo not found')
+    crud.delete_todo(db=db, todo_id=todo_id)
+    return {'message': f'ToDo with id:{todo_id} has been deleted'}
